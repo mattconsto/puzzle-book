49ce4a8a5f7b037ef506d89dfb7e1231bccf70ed
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-01-16 18:04:18 +0000

    PDF export

--- Makefile
+++ Makefile
@@ -1,11 +1,33 @@
 all:
-	rm -f book.txt
+	# Tidy
+	rm -f book.html
 	rm -f book.pdf
+	rm -f merged.pdf
+	rm -f output.pdf
 	rm -rf text2pdf.*
-	python3 generate.py > book.txt
-	pandoc -V papersize:a5 book.txt --pdf-engine=xelatex -o book.pdf
+
+	# Generate Book
+	python3 generate.py > book.html
+
+	# Convert to PDF
+	pandoc --pdf-engine=lualatex --template=template.latex -V documentclass=report -V papersize:a5 -V geometry:margin=2cm -f html -t latex book.html -o book.tex
+	sed -r -i 's/\\begin\{verbatim\}/\\begin\{lstlisting\}/g' book.tex
+	sed -r -i 's/\\end\{verbatim\}/\\end\{lstlisting\}/g' book.tex
+	sed -r -i 's/\\includegraphics\{/\\includegraphics[width=\\maxwidth\{\\textwidth\}]\{/g' book.tex
+	# sed -r -i 's/KwoC9R4okhg5qP1VqFU2spoileropenC7zVHSK1ZM3Jvkg9atqK/\\begin{rotate}{180}/g' book.tex
+	# sed -r -i 's/KwoC9R4okhg5qP1VqFU2spoilercloseC7zVHSK1ZM3Jvkg9atqK/\\end{rotate}/g' book.tex
+	pandoc --pdf-engine=lualatex --template=template.latex -V documentclass=report -V papersize:a5 -V geometry:margin=2cm -f latex book.tex -o book.pdf
+
+	# Merge with cover
+	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=merged.pdf cover.pdf book.pdf
+
+cover:
+	convert cover.png cover.pdf
 
 clean:
-	rm -f book.txt
+	# Tidy
+	rm -f book.html
 	rm -f book.pdf
+	rm -f merged.pdf
+	rm -f output.pdf
 	rm -rf text2pdf.*
--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import csv
+# https://unsplash.com/search/photos/puzzle
+
+import csv, re
 
 class DictEnum():
 	data = {}
@@ -12,25 +14,22 @@ class DictEnum():
 	def __getattr__(self, name):
 		return self.data[name]
 
-puzzleLimit = 5
+puzzleLimit = 50
 
-print("<style>.spoiler {background: #ccc; color: transparent;} .spoiler:hover {color: black;}</style>\n")
-
-print("The (Unoffical) Stack Exchange Puzzle Book\n==========================================\n\n* %d of the best puzzles and solutions submitted to puzzling.SE.\n* Arranged by [Matthew Consterdine](https://consto.uk/).\n* [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/).\n\n***\n" % puzzleLimit)
+print("<!DOCTYPE html><html><head><title>The Unofficial Stack Exchange Puzzle Book</title><style>ul {margin-left: 1em;} div {page-break-after: always;} img {max-width: 100%;} .spoiler {transform: rotate(180deg);} body {max-width: 50em; margin: 0 auto; background: #f4f4f4; color: #111111;}</style></head><body><h1>Acknowledgements</h1><p>I would like to thank the [puzzling.stackexchange.com](https://puzzling.stackexchange.com/) community for writing these puzzles, [stackexchange.com](https://stackexchange.com) for providing the data, and [creativecommons.org](https://creativecommons.org/) for making this possible. This book, like the puzzles within is licensed under the CC BY-SA 3.0 license.</p><p>This book was created using (data.stackexchange.com)[https://data.stackexchange.com] to gather data, [python](https://www.python.org/) to parse and structure the data, and [pandoc](https://pandoc.org/MANUAL.html) to typeset as pdf.</p><p>And lastly, thank you, the reader for reading this 'book'. I hope you enjoy the puzzles. If you wish to contribute to this book, it can be found on [my Github](https://github.com/mattconsto/puzzle-book).</p>\n")
 
 headers = DictEnum(["Id", "URL", "Score", "ViewCount", "AnswerCount", "FavoriteCount", "Title", "Tags", "Body", "CreationDate", "OwnerUserId", "DisplayName", "Reputation", "Gold", "Silver", "Bronze", "AnswerId", "AnswerScore", "AnswerBody", "AnswerCreationDate", "AnswerOwnerId", "AnswerDisplayName", "AnswerReputation", "AnswerGold", "AnswerSilver", "AnswerBronze"])
 
-with open("data.csv") as handle:
+with open("data.csv", encoding = "utf-8") as handle:
 	reader = csv.reader(handle, delimiter = ",", quotechar = "\"")
 	ignored = next(reader, None)
 	# @Improvement Check headers
 
-	print("# Contents\n")
+	print("<div><h1>Contents</h1>\n<p>Questions are sorted by score</p>\n<ul>")
 
 	count = 0
 	for row in reader:
-		# @Improvement replace h2>h3, h3>h4 and so on to respect ordering
-		print("* %s Votes - [%s](#question%s)" % (row[headers.Score], row[headers.Title], row[headers.Id]))
+		print("<li>%s: <a href=\"#question%s\">%s</a></li>" % (row[headers.Score], row[headers.Id], row[headers.Title]))
 
 		count += 1
 		if count >= puzzleLimit:
@@ -41,12 +40,24 @@ with open("data.csv") as handle:
 	ignored = next(reader, None)
 	# @Improvement Check headers
 
-	print("\n***\n\n# Puzzles\n")
+	print("</ul>\n</div><div>\n\n<h1>Puzzles</h1>\n")
 
 	count = 0
 	for row in reader:
-		# @Improvement replace h2>h3, h3>h4 and so on to respect ordering
-		print("## <a name=\"question%s\"></a>%s Votes - [%s](%s)\n\n~~Asked by [%s (%srep)](https://puzzling.stackexchange.com/users/%s) on %s~~\n\n~~Answered by [%s (%srep)](https://puzzling.stackexchange.com/users/%s) on %s~~\n\n%s\n[Go to Solution](#solution%s)\n\n***\n" % (row[headers.Id], row[headers.Score], row[headers.Title], row[headers.URL], row[headers.DisplayName], row[headers.Reputation], row[headers.OwnerUserId], row[headers.CreationDate], row[headers.AnswerDisplayName], row[headers.AnswerReputation], row[headers.AnswerOwnerId], row[headers.AnswerCreationDate], row[headers.Body], row[headers.Id]))
+		body = row[headers.Body]
+		mapping = {
+			'<H5>':'<H6>', '</H5>':'</H6>', '<h5>':'<h6>', '</h5>':'</h6>',
+			'<H4>':'<H5>', '</H4>':'</H5>', '<h4>':'<h5>', '</h4>':'</h5>',
+			'<H3>':'<H4>', '</H3>':'</H4>', '<h3>':'<h4>', '</h3>':'</h4>',
+			'<H2>':'<H3>', '</H2>':'</H3>', '<h2>':'<h3>', '</h2>':'</h3>',
+			'<H1>':'<H2>', '</H1>':'</H2>', '<h1>':'<h2>', '</h1>':'</h2>',
+		}
+		for k, v in mapping.items():
+			body = body.replace(k, v)
+
+		# body = re.sub(r'<blockquote class="spoiler">(.*)</blockquote>', r'KwoC9R4okhg5qP1VqFU2spoileropenC7zVHSK1ZM3Jvkg9atqK<blockquote>\1</blockquote>KwoC9R4okhg5qP1VqFU2spoilercloseC7zVHSK1ZM3Jvkg9atqK', body, flags=re.MULTILINE|re.DOTALL)
+
+		print("<h2><a name=\"question%s\">%s</a>: <a href=\"%s\">%s</a></h2>\n\n<p class=\"subtitle\">Asked by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a>, answered by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a></p>\n\n%s\n<a href=\"#solution%s\">Go to Solution</a>\n\n<hr />\n" % (row[headers.Id], row[headers.Score], row[headers.URL], row[headers.Title], row[headers.OwnerUserId], row[headers.DisplayName], row[headers.OwnerUserId], row[headers.AnswerOwnerId], row[headers.AnswerDisplayName], row[headers.AnswerOwnerId], body, row[headers.Id]))
 
 		count += 1
 		if count >= puzzleLimit:
@@ -57,14 +68,27 @@ with open("data.csv") as handle:
 	ignored = next(reader, None)
 	# @Improvement Check headers
 
-	print("# Solutions\n")
+	print("\n</div><div>\n<h1>Solutions</h1>\n")
 
 	count = 0
 	for row in reader:
-		print("## <a name=\"solution%s\"></a>%s Votes - [%s](%s)\n\n~~Asked by [%s (%srep)](https://puzzling.stackexchange.com/users/%s) on %s~~\n\n~~Answered by [%s (%srep)](https://puzzling.stackexchange.com/users/%s) on %s~~\n\n%s\n[Return to Question](#question%s)\n\n***\n" % (row[headers.Id], row[headers.Score], row[headers.Title], row[headers.URL], row[headers.DisplayName], row[headers.Reputation], row[headers.OwnerUserId], row[headers.CreationDate], row[headers.AnswerDisplayName], row[headers.AnswerReputation], row[headers.AnswerOwnerId], row[headers.AnswerCreationDate], row[headers.AnswerBody], row[headers.Id]))
+		body = row[headers.AnswerBody]
+		mapping = {
+			'<H5>':'<H6>', '</H5>':'</H6>', '<h5>':'<h6>', '</h5>':'</h6>',
+			'<H4>':'<H5>', '</H4>':'</H5>', '<h4>':'<h5>', '</h4>':'</h5>',
+			'<H3>':'<H4>', '</H3>':'</H4>', '<h3>':'<h4>', '</h3>':'</h4>',
+			'<H2>':'<H3>', '</H2>':'</H3>', '<h2>':'<h3>', '</h2>':'</h3>',
+			'<H1>':'<H2>', '</H1>':'</H2>', '<h1>':'<h2>', '</h1>':'</h2>',
+		}
+		for k, v in mapping.items():
+			body = body.replace(k, v)
+
+		# body = re.sub(r'<blockquote class="spoiler">(.*)</blockquote>', r'KwoC9R4okhg5qP1VqFU2spoileropenC7zVHSK1ZM3Jvkg9atqK<blockquote>\1</blockquote>KwoC9R4okhg5qP1VqFU2spoilercloseC7zVHSK1ZM3Jvkg9atqK', body, flags=re.MULTILINE|re.DOTALL)
+
+		print("<h2><a name=\"solution%s\">%s</a>: <a href=\"%s\">%s</a></h2>\n\n<p class=\"subtitle\">Asked by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a>, answered by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a></p>\n\n%s\n<a href=\"#question%s\">Return to Question</a>\n\n<hr />\n" % (row[headers.Id], row[headers.Score], row[headers.URL], row[headers.Title], row[headers.OwnerUserId], row[headers.DisplayName], row[headers.OwnerUserId], row[headers.AnswerOwnerId], row[headers.AnswerDisplayName], row[headers.AnswerOwnerId], body, row[headers.Id]))
 
 		count += 1
 		if count >= puzzleLimit:
 			break
 
-	print("Thanks for reading!")
+	print("</div></body></html>")
