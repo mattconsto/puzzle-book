ced74b22c3f0d49e45bed74cecc268261e841301
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-01-20 21:14:34 +0000

    Significant improvements, all that's really left is getting maths to work

--- Makefile
+++ Makefile
@@ -1,33 +1,50 @@
-all:
-	# Tidy
-	rm -f book.html
-	rm -f book.pdf
-	rm -f merged.pdf
-	rm -f output.pdf
-	rm -rf text2pdf.*
+all: clean cover generate tex pdf
 
+generate:
 	# Generate Book
 	python3 generate.py > book.html
 
+tex:
 	# Convert to PDF
-	pandoc --pdf-engine=lualatex --template=template.latex -V documentclass=report -V papersize:a5 -V geometry:margin=2cm -f html -t latex book.html -o book.tex
+	# pandoc --pdf-engine=lualatex --number-sections --top-level-division=chapter --template=template.latex -V toc -V toc-depth=3 -V documentclass=extreport -V fontsize=9pt -V papersize:a5 -V geometry:margin=0.5in -f html -t latex book.html -o book.tex
+	pandoc --pdf-engine=lualatex --number-sections --top-level-division=chapter --template=template.latex -V toc -V toc-depth=1 -V documentclass=extreport -V fontsize=9pt -V papersize:a5 -V geometry:margin=0.5in -f html -t latex book_edited.html -o book.tex
+	# Unfinished work, trying to get maths to work.
+	# pandoc --pdf-engine=lualatex --number-sections --top-level-division=chapter --template=template.latex -V toc -V toc-depth=1 -V documentclass=extreport -V fontsize=9pt -V papersize:a5 -V geometry:margin=0.5in -f markdown+raw_tex -t latex book.md -o book.tex
+	# perl -plne 's/\\\$/\$/g' book.tex > temp.tex
+	# perl -plne 's/\\\\/\\/g' temp.tex > book.tex
+
+	# Tidy
+	dos2unix book.tex
 	sed -r -i 's/\\begin\{verbatim\}/\\begin\{lstlisting\}/g' book.tex
 	sed -r -i 's/\\end\{verbatim\}/\\end\{lstlisting\}/g' book.tex
-	sed -r -i 's/\\includegraphics\{/\\includegraphics[width=\\maxwidth\{\\textwidth\}]\{/g' book.tex
-	# sed -r -i 's/KwoC9R4okhg5qP1VqFU2spoileropenC7zVHSK1ZM3Jvkg9atqK/\\begin{rotate}{180}/g' book.tex
-	# sed -r -i 's/KwoC9R4okhg5qP1VqFU2spoilercloseC7zVHSK1ZM3Jvkg9atqK/\\end{rotate}/g' book.tex
-	pandoc --pdf-engine=lualatex --template=template.latex -V documentclass=report -V papersize:a5 -V geometry:margin=2cm -f latex book.tex -o book.pdf
 
+	# New page
+	sed -r -i -z 's/\\begin\{center\}\\rule\{0.5\\linewidth\}\{\\linethickness\}\\end\{center\}\n+\\begin\{center\}\\rule\{0.5\\linewidth\}\{\\linethickness\}\\end\{center\}/\\begin\{center\}\\rule\{1.0\\linewidth\}\{\\linethickness\}\\end\{center\}/g' book.tex
+
+	# Make pagerefs
+	sed -r -i 's/\\protect\\hypertarget\{question([0-9]+)\}\{\}\{\}/\\label\{question\1\}/g' book.tex
+	sed -r -i 's/\\protect\\hypertarget\{solution([0-9]+)\}\{\}\{\}/\\label\{solution\1\}/g' book.tex
+	sed -r -i 's/\\protect\\hyperlink\{solution([0-9]+)\}\{[a-zA-Z0-9\-_ ]+\}/Solution on page \\pageref\{solution\1\}./g' book.tex
+	sed -r -i 's/\\protect\\hyperlink\{question([0-9]+)\}\{[a-zA-Z0-9\-_ ]+\}/Question on page \\pageref\{question\1\}./g' book.tex
+	sed -r -i 's/\\href\{question([0-9]+)\}\{[a-zA-Z0-9\-_ ]+\}/\\pageref\{question\1\}/g' book.tex
+
+pdf:
+	# To pdf
+	lualatex book.tex
+	lualatex book.tex
+	lualatex book.tex
+
+	make merge
+
+merge:
 	# Merge with cover
 	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=merged.pdf cover.pdf book.pdf
 
 cover:
-	convert cover.png cover.pdf
+	# Cover
+	convert -density 300 cover.png cover.pdf
 
 clean:
 	# Tidy
-	rm -f book.html
-	rm -f book.pdf
-	rm -f merged.pdf
-	rm -f output.pdf
+	rm -f book.aux book.log book.out book.tex.bak book.toc book.pdf output.pdf
 	rm -rf text2pdf.*
--- generate.py
+++ generate.py
@@ -2,7 +2,8 @@
 
 # https://unsplash.com/search/photos/puzzle
 
-import csv, re
+import csv, re, subprocess
+from urllib import request
 
 class DictEnum():
 	data = {}
@@ -14,33 +15,20 @@ class DictEnum():
 	def __getattr__(self, name):
 		return self.data[name]
 
-puzzleLimit = 50
+puzzleLimit = 100
 
-print("<!DOCTYPE html><html><head><title>The Unofficial Stack Exchange Puzzle Book</title><style>ul {margin-left: 1em;} div {page-break-after: always;} img {max-width: 100%;} .spoiler {transform: rotate(180deg);} body {max-width: 50em; margin: 0 auto; background: #f4f4f4; color: #111111;}</style></head><body><h1>Acknowledgements</h1><p>I would like to thank the [puzzling.stackexchange.com](https://puzzling.stackexchange.com/) community for writing these puzzles, [stackexchange.com](https://stackexchange.com) for providing the data, and [creativecommons.org](https://creativecommons.org/) for making this possible. This book, like the puzzles within is licensed under the CC BY-SA 3.0 license.</p><p>This book was created using (data.stackexchange.com)[https://data.stackexchange.com] to gather data, [python](https://www.python.org/) to parse and structure the data, and [pandoc](https://pandoc.org/MANUAL.html) to typeset as pdf.</p><p>And lastly, thank you, the reader for reading this 'book'. I hope you enjoy the puzzles. If you wish to contribute to this book, it can be found on [my Github](https://github.com/mattconsto/puzzle-book).</p>\n")
+print("<!DOCTYPE html><html><head><title>The Unofficial Stack Exchange Puzzle Book</title><style>ul {margin-left: 1em;} div {page-break-after: always;} img {max-width: 100%;} .spoiler {transform: rotate(180deg);} body {max-width: 50em; margin: 0 auto; background: #f4f4f4; color: #111111;}</style></head><body><h1>Acknowledgements</h1><p>I would like to thank the <a href=\"https://puzzling.stackexchange.com/\">puzzling.stackexchange.com</a> community for writing these puzzles, <a href=\"https://stackexchange.com\">stackexchange.com</a> for providing the data, and <a href=\"https://creativecommons.org/\">creativecommons.org</a> for making this possible. Like the puzzles within, this is licensed under the CC BY-SA 3.0 license. This book was created using <a href=\"https://data.stackexchange.com\">data.stackexchange.com</a> to gather data, <a href=\"https://www.python.org/\">python</a> to parse and structure the data, and <a href=\"https://pandoc.org/MANUAL.html\">pandoc</a> to typeset as pdf.</p><p>And thank you, the reader for reading this 'book'. I hope you enjoy the puzzles. If you wish to contribute to this book, it can be found on <a href=\"https://github.com/mattconsto/puzzle-book\">github.com/mattconsto/puzzle-book</a>.</p><p>Happy puzzling!</p>\n")
 
 headers = DictEnum(["Id", "URL", "Score", "ViewCount", "AnswerCount", "FavoriteCount", "Title", "Tags", "Body", "CreationDate", "OwnerUserId", "DisplayName", "Reputation", "Gold", "Silver", "Bronze", "AnswerId", "AnswerScore", "AnswerBody", "AnswerCreationDate", "AnswerOwnerId", "AnswerDisplayName", "AnswerReputation", "AnswerGold", "AnswerSilver", "AnswerBronze"])
 
-with open("data.csv", encoding = "utf-8") as handle:
-	reader = csv.reader(handle, delimiter = ",", quotechar = "\"")
-	ignored = next(reader, None)
-	# @Improvement Check headers
-
-	print("<div><h1>Contents</h1>\n<p>Questions are sorted by score</p>\n<ul>")
-
-	count = 0
-	for row in reader:
-		print("<li>%s: <a href=\"#question%s\">%s</a></li>" % (row[headers.Score], row[headers.Id], row[headers.Title]))
-
-		count += 1
-		if count >= puzzleLimit:
-			break
+index = {}
 
 with open("data.csv") as handle:
 	reader = csv.reader(handle, delimiter = ",", quotechar = "\"")
 	ignored = next(reader, None)
 	# @Improvement Check headers
 
-	print("</ul>\n</div><div>\n\n<h1>Puzzles</h1>\n")
+	print("<div>\n\n<h1>Puzzles</h1>\n")
 
 	count = 0
 	for row in reader:
@@ -55,13 +43,30 @@ with open("data.csv") as handle:
 		for k, v in mapping.items():
 			body = body.replace(k, v)
 
-		# body = re.sub(r'<blockquote class="spoiler">(.*)</blockquote>', r'KwoC9R4okhg5qP1VqFU2spoileropenC7zVHSK1ZM3Jvkg9atqK<blockquote>\1</blockquote>KwoC9R4okhg5qP1VqFU2spoilercloseC7zVHSK1ZM3Jvkg9atqK', body, flags=re.MULTILINE|re.DOTALL)
+		# Download images
+		def replacement(match):
+			url = match.group(1)
+			filename = url[url.rfind("/")+1:]
+			request.urlretrieve(url, "images/" + filename)
+			if filename[filename.rfind(".")+1:] == "gif":
+				subprocess.check_call(["convert", "images/" + filename + "[0]", "images/" + filename[:filename.rfind(".")] + ".png"]);
+				filename = filename[:filename.rfind(".")] + ".png"
+			return "<img src=\"images/" + filename + "\""
 
-		print("<h2><a name=\"question%s\">%s</a>: <a href=\"%s\">%s</a></h2>\n\n<p class=\"subtitle\">Asked by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a>, answered by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a></p>\n\n%s\n<a href=\"#solution%s\">Go to Solution</a>\n\n<hr />\n" % (row[headers.Id], row[headers.Score], row[headers.URL], row[headers.Title], row[headers.OwnerUserId], row[headers.DisplayName], row[headers.OwnerUserId], row[headers.AnswerOwnerId], row[headers.AnswerDisplayName], row[headers.AnswerOwnerId], body, row[headers.Id]))
+		body = re.sub(r'<img src="(http.+?\.[a-zA-Z]+)"', replacement, body)
+
+		print("<h2><a name=\"question%s\"></a><a href=\"%s\">%s</a></h2>\n\n<p class=\"subtitle\">%s votes, asked by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a>, answered by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a></p>\n\n%s\n<a href=\"#solution%s\">Go to Solution</a>\n\n<hr /><hr />\n" % (row[headers.Id], row[headers.URL], row[headers.Title], row[headers.Score], row[headers.OwnerUserId], row[headers.DisplayName], row[headers.OwnerUserId], row[headers.AnswerOwnerId], row[headers.AnswerDisplayName], row[headers.AnswerOwnerId], body, row[headers.Id]))
 
 		count += 1
 		if count >= puzzleLimit:
 			break
+
+		# Generate index
+		tags = [tag[1:-1] for tag in re.findall(r'<[a-z0-9\-_]+>', row[headers.Tags])]
+		for tag in tags:
+			if not tag in index:
+				index[tag] = []
+			index[tag].append(row[headers.Id])
 
 with open("data.csv") as handle:
 	reader = csv.reader(handle, delimiter = ",", quotechar = "\"")
@@ -83,12 +88,35 @@ with open("data.csv") as handle:
 		for k, v in mapping.items():
 			body = body.replace(k, v)
 
-		# body = re.sub(r'<blockquote class="spoiler">(.*)</blockquote>', r'KwoC9R4okhg5qP1VqFU2spoileropenC7zVHSK1ZM3Jvkg9atqK<blockquote>\1</blockquote>KwoC9R4okhg5qP1VqFU2spoilercloseC7zVHSK1ZM3Jvkg9atqK', body, flags=re.MULTILINE|re.DOTALL)
+		# Download images
+		def replacement(match):
+			url = match.group(1)
+			filename = url[url.rfind("/")+1:]
+			request.urlretrieve(url, "images/" + filename)
+			if filename[filename.rfind(".")+1:] == "gif":
+				subprocess.check_call(["convert", "images/" + filename + "[0]", "images/" + filename[:filename.rfind(".")] + ".png"]);
+				filename = filename[:filename.rfind(".")] + ".png"
+			return "<img src=\"images/" + filename + "\""
 
-		print("<h2><a name=\"solution%s\">%s</a>: <a href=\"%s\">%s</a></h2>\n\n<p class=\"subtitle\">Asked by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a>, answered by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a></p>\n\n%s\n<a href=\"#question%s\">Return to Question</a>\n\n<hr />\n" % (row[headers.Id], row[headers.Score], row[headers.URL], row[headers.Title], row[headers.OwnerUserId], row[headers.DisplayName], row[headers.OwnerUserId], row[headers.AnswerOwnerId], row[headers.AnswerDisplayName], row[headers.AnswerOwnerId], body, row[headers.Id]))
+		body = re.sub(r'<img src="(http.+?\.[a-zA-Z]+)"', replacement, body)
+
+		print("<h2><a name=\"solution%s\"></a><a href=\"%s\">%s</a></h2>\n\n<p class=\"subtitle\">%s Votes, asked by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a>, answered by <a href=\"https://puzzling.stackexchange.com/users/%s\">%s#%s</a></p>\n\n%s\n<a href=\"#question%s\">Return to Question</a>\n\n<hr /><hr />\n" % (row[headers.Id], row[headers.URL], row[headers.Title], row[headers.Score], row[headers.OwnerUserId], row[headers.DisplayName], row[headers.OwnerUserId], row[headers.AnswerOwnerId], row[headers.AnswerDisplayName], row[headers.AnswerOwnerId], body, row[headers.Id]))
 
 		count += 1
 		if count >= puzzleLimit:
 			break
 
-	print("</div></body></html>")
+print("\n</div><div>\n<h1>Index</h1>\n<ul>")
+
+for tag in sorted(index.keys()):
+	first = True
+	concat = ""
+	for i in index[tag]:
+		if first:
+			first = False
+		else:
+			concat += ", "
+		concat += "<a href=\"question%s\">Q%s</a>" % (i, i)
+	print("<li>%s: %s.</li>" % (tag.title(), concat))
+
+print("</ul>\n</div><div>\n</body></html>")
